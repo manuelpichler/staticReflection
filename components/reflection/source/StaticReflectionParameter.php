8fb4ed0f1f8ad9a743e4ca894e09cd86c9205138
Manuel Pichler <mapi@manuel-pichler.de>
2009-10-20 18:51:54 +0000

    # Parameter skeleton added.
    git-svn-id: svn://xplib.de/staticReflection/trunk@28 43604d11-ced7-43f5-a9b8-bcb6bf537261

---
+++ components/reflection/source/StaticReflectionParameter.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * I provide completely working code with this framework, which will not be
+ * developed any further, because there are already existing packages, which try
+ * to provide similar functionallities.
+ */
+
+namespace de\buzz2ee\reflection;
+
+/**
+ * Static parameter implementation.
+ *
+ * @author  Manuel Pichler <mapi@pdepend.org>
+ * @license Copyright by Manuel Pichler
+ * @version $Revision$
+ */
+class StaticReflectionParameter extends \ReflectionParameter
+{
+    const TYPE = __CLASS__;
+
+    public function __construct()
+    {
+
+    }
+
+    public function getName()
+    {
+        
+    }
+
+    public function getPosition()
+    {
+        
+    }
+
+    public function allowsNull()
+    {
+        
+    }
+
+    public function getClass()
+    {
+
+    }
+
+    public function getDeclaringClass()
+    {
+        
+    }
+
+    public function getDeclaringFunction()
+    {
+        
+    }
+
+    public function getDefaultValue()
+    {
+
+    }
+
+    public function isArray()
+    {
+
+    }
+
+    public function isDefaultValueAvailable()
+    {
+
+    }
+
+    public function isOptional()
+    {
+
+    }
+
+    public function isPassedByReference()
+    {
+        
+    }
+
+    public function __toString()
+    {
+        
+    }
+}
