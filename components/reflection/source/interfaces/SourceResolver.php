d89451f3445c5ba2b659d032ef77f70e13fdf17a
Manuel Pichler <mapi@manuel-pichler.de>
2009-11-07 12:48:23 +0000

    # Missing class doc comment added.
    git-svn-id: svn://xplib.de/staticReflection/trunk@143 43604d11-ced7-43f5-a9b8-bcb6bf537261

--- components/reflection/source/interfaces/SourceResolver.php
+++ components/reflection/source/interfaces/SourceResolver.php
@@ -50,9 +50,13 @@ namespace org\pdepend\reflection\interfaces;
 /**
  * Base interface for a source resolver.
  *
- * @author  Manuel Pichler <mapi@pdepend.org>
- * @license Copyright by Manuel Pichler
- * @version $Revision$
+ * @category  PHP
+ * @package   org\pdepend\reflection\interfaces
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2009 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://pdepend.org/
  */
 interface SourceResolver
 {
