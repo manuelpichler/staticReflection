20a2220db1bf20e98aa7b86a87241e38dbaac64c
Manuel Pichler <mapi@manuel-pichler.de>
2009-11-08 12:20:28 +0000

    # Missing documentation added.
    git-svn-id: svn://xplib.de/staticReflection/trunk@156 43604d11-ced7-43f5-a9b8-bcb6bf537261

--- components/reflection/source/ReflectionClassProxyContext.php
+++ components/reflection/source/ReflectionClassProxyContext.php
@@ -1,20 +1,92 @@
 <?php
+/**
+ * This file is part of the static reflection component.
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2008-2009, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  PHP
+ * @package   org\pdepend\reflection
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2009 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://pdepend.org/
+ */
+
 namespace org\pdepend\reflection;
 
 use org\pdepend\reflection\interfaces\ParserContext;
 
+/**
+ * Simple parser context implementation that will return a proxy instance for
+ * each requested reflection class.
+ *
+ * @category  PHP
+ * @package   org\pdepend\reflection
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2009 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://pdepend.org/
+ */
 class ReflectionClassProxyContext implements ParserContext
 {
+    /**
+     * The currently used reflection session.
+     *
+     * @var \org\pdepend\reflection\ReflectionSession
+     */
     private $_session = null;
 
+    /**
+     * Constructs a new parser context instance.
+     *
+     * @param \org\pdepend\reflection\ReflectionSession $session The currently
+     *        used reflection session instance which has created this parser
+     *        context.
+     */
     public function __construct( ReflectionSession $session )
     {
         $this->_session = $session;
     }
 
     /**
+     * This method will be called by the reflection parser for all found classes
+     * and/or interfaces that the currently parsed class depends on.
      *
-     * @param string $className
+     * @param string $className Full qualified name of the request class.
      *
      * @return \ReflectionClass
      */
--- components/reflection/source/interfaces/ParserContext.php
+++ components/reflection/source/interfaces/ParserContext.php
@@ -3,5 +3,13 @@ namespace org\pdepend\reflection\interfaces;
 
 interface ParserContext
 {
+    /**
+     * This method will be called by the reflection parser for all found classes
+     * and/or interfaces that the currently parsed class depends on.
+     *
+     * @param string $className Full qualified name of the request class.
+     *
+     * @return \ReflectionClass
+     */
     function getClass( $className );
 }
