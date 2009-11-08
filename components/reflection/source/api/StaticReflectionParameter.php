2c6802af815f2602c514854bfb7268b99149e239
Manuel Pichler <mapi@manuel-pichler.de>
2009-11-08 21:31:04 +0000

    # Inline documentation completed.
    git-svn-id: svn://xplib.de/staticReflection/trunk@177 43604d11-ced7-43f5-a9b8-bcb6bf537261

--- components/reflection/source/api/StaticReflectionParameter.php
+++ components/reflection/source/api/StaticReflectionParameter.php
@@ -50,9 +50,13 @@ namespace org\pdepend\reflection\api;
 /**
  * Static parameter implementation.
  *
- * @author  Manuel Pichler <mapi@pdepend.org>
- * @license Copyright by Manuel Pichler
- * @version $Revision$
+ * @category  PHP
+ * @package   org\pdepend\reflection\api
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2009 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://pdepend.org/
  */
 class StaticReflectionParameter extends \ReflectionParameter
 {
@@ -420,6 +424,11 @@ class StaticReflectionParameter extends \ReflectionParameter
         }
     }
 
+    /**
+     * Returns a string representation of this property.
+     *
+     * @return string
+     */
     public function __toString()
     {
         
