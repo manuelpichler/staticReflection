3ebb4a37b3f751b9c04f2088fe0e2d1a38614584
Manuel Pichler <mapi@manuel-pichler.de>
2009-11-08 21:30:40 +0000

    # Documentation of exception class completed.
    git-svn-id: svn://xplib.de/staticReflection/trunk@168 43604d11-ced7-43f5-a9b8-bcb6bf537261

--- components/reflection/source/exceptions/ParserException.php
+++ components/reflection/source/exceptions/ParserException.php
@@ -50,9 +50,13 @@ namespace org\pdepend\reflection\exceptions;
 /**
  * Base class for parser related exceptions.
  *
- * @author  Manuel Pichler <mapi@pdepend.org>
- * @license Copyright by Manuel Pichler
- * @version $Revision$
+ * @category  PHP
+ * @package   org\pdepend\reflection\exceptions
+ * @author    Manuel Pichler <mapi@pdepend.org>
+ * @copyright 2008-2009 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://pdepend.org/
  */
 class ParserException extends \ReflectionException
 {
