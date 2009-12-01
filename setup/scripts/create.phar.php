bd9ae4ccb92378eb24d639cc6f9ac84d9c8f0212
Manuel Pichler <mapi@manuel-pichler.de>
2009-12-01 07:00:36 +0000

    # Bootstrap/Stub handling for the phar distribution added.
    git-svn-id: svn://xplib.de/staticReflection/trunk@233 43604d11-ced7-43f5-a9b8-bcb6bf537261

--- setup/scripts/create.phar.php
+++ setup/scripts/create.phar.php
@@ -1,9 +1,26 @@
 #!/usr/bin/env php
 <?php
 
+if ( count( $argv ) < 4 )
+{
+    echo 'Usage: create.phar.php <project> <output-file> <input-dir> [<bootstrap>]', PHP_EOL;
+    exit( 1 );
+}
+
 $project = $argv[1] . '.phar';
-$output  = realpath( $argv[3] ) . '/' . $argv[2] . '.phar';
-$input   = realpath( $argv[4] );
+$output  = $argv[2];
+$input   = realpath( $argv[3] );
+
+if ( isset( $argv[4] ) && trim( $argv[4] ) !== '' )
+{
+    $stub = file_get_contents( $argv[4] );
+}
+else
+{
+    $stub = '<?php Phar::mapPhar("${archive.alias}"); __HALT_COMPILER(); ?>';
+}
 
 $phar = new Phar( $output, 0, $project );
 $phar->buildFromDirectory( $input );
+$phar->setStub( str_replace( '${archive.alias}', $project, $stub ) );
+$phar->compressFiles( Phar::GZ );
