c64c9b108f0e6c6f43dddd481ffa5ffca6475b4f
Manuel Pichler <mapi@manuel-pichler.de>
2010-02-26 10:16:38 +0100

    # Invalid test cases fixed.

--- source/pdepend/reflection/api/StaticReflectionMethod.php
+++ source/pdepend/reflection/api/StaticReflectionMethod.php
@@ -240,7 +240,7 @@ class StaticReflectionMethod extends \ReflectionMethod
 
         if ( ( $modifiers & ~$expected ) !== 0 )
         {
-            throw new \ReflectionException( 'Invalid method modifier given.' );
+            throw new \LogicException( 'Invalid method modifier given.' );
         }
         $this->_modifiers = $modifiers;
     }
